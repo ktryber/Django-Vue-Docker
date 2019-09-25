63e5b2024b5248537b88bd2919602f9defd59a1a
Kris Tryber <ktryber@gmail.com>
2019-09-25 06:42:28 -0700

    change directory back and add timeout flag

--- backend/scripts/gunicorn.sh
+++ backend/scripts/gunicorn.sh
@@ -6,4 +6,4 @@ set -o nounset
 
 python manage.py migrate
 python manage.py collectstatic --noinput --verbosity 0
-gunicorn config.wsgi -w 4 --worker-class gevent -b 0.0.0.0:8000 --chdir=/var/app/current/backend
+gunicorn config.wsgi -w 4 --worker-class gevent -b 0.0.0.0:8000 --timeout 120 --chdir=/app
